0c77cb1514b9186e14a5303991e9bb842ab80b95
Maung-Thura <maungthura2024@u.northwestern.edu>
2024-11-16 18:51:34 -0600

    lab 4 version of sending telemetries to Azure

---
+++ Rfs2Module/test/test_main.sh
@@ -0,0 +1,10 @@
+#export IOTEDGE_IOTHUBHOSTNAME="IOTEDGE"
+
+#export IOTHUB_DEVICE_SECURITY_TYPE="DPS"
+#export IOTHUB_DEVICE_DPS_ENDPOINT="global.azure-devices-provisioning.net"
+#export IOTHUB_DEVICE_DPS_ID_SCOPE=""
+#export IOTHUB_DEVICE_DPS_DEVICE_ID=""
+#export IOTHUB_DEVICE_DPS_DEVICE_KEY=""
+
+export IOTHUB_DEVICE_SECURITY_TYPE="connectionString"
+export IOTHUB_DEVICE_CONNECTION_STRING="HostName=maung-thu-ra-hub.azure-devices.net;DeviceId=de10nano-mthura;SharedAccessKey=eTjcn/zI451i8e+D1x7DpQM8d0QzvQ2Ts2uhC9KmigA="
